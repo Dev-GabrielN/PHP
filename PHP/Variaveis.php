581d3a47c3a4fdece8cc3ba291fdf9c57536d495
Gabriel Nascimento de Souza <84488487+Dev-GabrielN@users.noreply.github.com>
2022-11-08 09:48:54 -0300

    Create Variaveis.php

---
+++ PHP/Variaveis.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style pattern/style.css">
+    <title>Variaveis</title>
+</head>
+
+<body>
+    
+    <?php
+    
+    $numero = 15;
+    $numero_2 = 16;
+    $resultado = $numero + $numero_2;
+
+    echo "O Resultado da soma de $numero + $numero_2 é de $resultado";
+    
+    
+    ?>
+
+</body>
+
+</html>
