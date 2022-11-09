10e619acb282b7b72e862884bbb4767b3c49c969
Gabriel Nascimento de Souza <84488487+Dev-GabrielN@users.noreply.github.com>
2022-11-09 12:10:18 -0300

    pequenos updates

--- PHP/Hello_World.php
+++ PHP/Hello_World.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,10 +8,14 @@
     <link rel="stylesheet" href="../PHP/style pattern/style.css">
     <title>Hello World</title>
 </head>
+
 <body>
+    
     <?php
     echo "Hello World! (echo)<br>";
     print "Hello World! (print)";
-?>
+    ?>
+
 </body>
+
 </html>
--- PHP/Variaveis.php
+++ PHP/Variaveis.php
@@ -12,15 +12,21 @@
 <body>
 
     <?php
+    // A tipagem é dinamica, então o valor é interpretado pelo php e 
+    // o proprio faz a tipagem, porém é possivel fazer especicações.
+
+    $nome = (string)"arroz";
+    $numero = (float)5.98;
 
     $numero_int = 15;
     $string = "Arroz com peixe";
     $numero_float = 15.23;
-    $Bolean = true or false;
+    $Boolean = true; // (true = 1) ou (false = vazio)
 
-?>
-
-    
+    $nome = "Gabriel";
+    $Gabriel = "biel";
+    echo $$nome;
+    ?>
 
 </body>
 
